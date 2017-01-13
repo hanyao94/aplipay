a4ea450e3c877a2529039435192622dd45172bd8
Mr.Seven <hanyao_huang@163.com>
2017-01-13 18:51:05 +0800

    0.2v--支付宝回调函数第二步，验签完成

--- aplipay_notify.php
+++ aplipay_notify.php
@@ -6,7 +6,10 @@
  * Time: 14:36
  * 支付宝回调地址
  */
-    include_once "lotusphp_runtime/Logger/Logger.php";
+    header("Content-type: text/html; charset=utf-8");
+    include_once dirname(__FILE__).DIRECTORY_SEPARATOR."lotusphp_runtime/Logger/Logger.php";
+    include_once dirname(__FILE__).DIRECTORY_SEPARATOR."aop/AopClient.php";
+    include_once dirname(__FILE__).DIRECTORY_SEPARATOR."f2fpay/config/config.php";
 
     $log = new LtLogger(); //日志
     $log->conf["log_file"] = "log/aplipay_notify_log.txt";//日志保存的位置
@@ -18,9 +21,24 @@
         }
         $log_param['time'] = "date: ".date("Y-m-d h:i:sa"); //时间
         $log->log($log_param); //写入日志文件
-        print_r($param);
+        //print_r($param);
     }else{
         $param = array("empty","date: ".date("Y-m-d h:i:sa"));
         $log->log($param);
-        echo "empty";
+        exit("failure");
     }
+
+    //验签
+    $aopClient = new AopClient();
+    $aopClient->alipayrsaPublicKey = $config['alipay_public_key']; //公钥
+    $signVerified = $aopClient->rsaCheckV1($param,'');
+    if ($signVerified){
+        $message = array("SUCCESS","date: ".date("Y-m-d h:i:sa"));
+        $log->log($message);
+        echo "SUCCESS";
+    }else{
+        exit("failure");
+    }
+
+
+
