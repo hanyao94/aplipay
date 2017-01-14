8e8676220fc9ab3d4551381683cdc88637b7515a
Mr.Seven <hanyao_huang@163.com>
2017-01-14 16:45:30 +0800

    0.7v--支付宝回调函数第七步，测试得知只有待付款和交易成功会回调，其他的不回调，那么就在这两个更改状态，去掉交易关闭的判断

--- aplipay_notify.php
+++ aplipay_notify.php
@@ -10,6 +10,7 @@
     include_once dirname(__FILE__).DIRECTORY_SEPARATOR."lotusphp_runtime/Logger/Logger.php";
     include_once dirname(__FILE__).DIRECTORY_SEPARATOR."aop/AopClient.php";
     include_once dirname(__FILE__).DIRECTORY_SEPARATOR."f2fpay/config/config.php";
+    define('LUYAN_EMAIL','eluyan@luyan.com.cn');
 
     $log = new LtLogger(); //日志
     $log->conf["log_file"] = "log/aplipay_notify_log.txt";//日志保存的位置
@@ -39,26 +40,36 @@
         //验证这笔订单和商户系统创建的订单是否一致，金额是否存在
         $log->log(array("订单号、金额验证通过","date: ".date("Y-m-d h:i:sa")));
 
+        //校验通知中的seller_id（或者seller_email) 是否为这笔单据的对应的操作方
+        if ($param['seller_email'] == LUYAN_EMAIL){
+            $log->log(array("收款账户为卖方账户","date: ".date("Y-m-d h:i:sa")));
+        }
 
+        /**
+         * 这边做个说明，只有 WAIT_BUYER_PAY 和 TRADE_SUCCESS 这两种状态会回调，
+         * 所以当交易超时未付款的时候，支付宝并不会回调通知交易已关闭，但是再用原来的订单号生成二维码则会出现交易已关闭无法支付
+         * 因此，解决的办法是，在生成支付宝订单时，先判断这笔订单的状态，如果是待付款，那就把订单号重新生成一个，才不会生成的无法支付
+         */
         //WAIT_BUYER_PAY 	交易创建，等待买家付款
         if ($param['trade_status'] == 'WAIT_BUYER_PAY'){
+            //等待付款，更新状态--待付款
             $log->log(array("交易创建成功，等待买家付款","date: ".date("Y-m-d h:i:sa")));
             exit("failure");
         }
 
         //根据支付状态trade_status，TRADE_SUCCESS或TRADE_FINISHED时，支付宝才会认定为买家付款成功。
         if (($param['trade_status'] == 'TRADE_SUCCESS')||($param['trade_status'] =='TRADE_FINISHED')){
-            //交易成功，更改状态
+            //交易成功，更改状态--已付款
             $log->log(array("交易成功，更改订单状态","trade_status: ".$param['trade_status'],"date: ".date("Y-m-d h:i:sa")));
             exit("SUCCESS");
         }
 
-        //TRADE_CLOSED     未付款交易超时关闭，或付完款全额退款
-        if ($param['trade_status'] == 'TRADE_CLOSED'){
-            //未付款超时，更改订单号，待重新发起支付
-            $log->log(array("未付款超时，更改订单号，待重新发起支付","date: ".date("Y-m-d h:i:sa")));
-            exit("failure");
-        }
+//        //TRADE_CLOSED     未付款交易超时关闭，或付完款全额退款，这个状态并不会回调
+//        if ($param['trade_status'] == 'TRADE_CLOSED'){
+//            //未付款超时，更改订单号，待重新发起支付
+//            $log->log(array("未付款超时，更改订单号，待重新发起支付","date: ".date("Y-m-d h:i:sa")));
+//            exit("failure");
+//        }
 
     }else{
         //验签失败
