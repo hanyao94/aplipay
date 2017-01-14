8e99837cce9eb0c346a086295f3823857bba881e
Mr.Seven <hanyao_huang@163.com>
2017-01-14 11:44:55 +0800

    0.3v--支付宝回调函数第三步，验签完成的业务逻辑，更改订单号或者执行其他操作

--- aplipay_notify.php
+++ aplipay_notify.php
@@ -23,7 +23,7 @@
         $log->log($log_param); //写入日志文件
         //print_r($param);
     }else{
-        $param = array("empty","date: ".date("Y-m-d h:i:sa"));
+        $param = array("支付宝post请求参数为空","date: ".date("Y-m-d h:i:sa"));
         $log->log($param);
         exit("failure");
     }
@@ -33,10 +33,36 @@
     $aopClient->alipayrsaPublicKey = $config['alipay_public_key']; //公钥
     $signVerified = $aopClient->rsaCheckV1($param,'');
     if ($signVerified){
-        $message = array("SUCCESS","date: ".date("Y-m-d h:i:sa"));
-        $log->log($message);
-        echo "SUCCESS";
+        //验签通过
+        $log->log(array("验签通过","date: ".date("Y-m-d h:i:sa")));
+
+        //验证这笔订单和商户系统创建的订单是否一致，金额是否存在
+        $log->log(array("订单号、金额验证通过","date: ".date("Y-m-d h:i:sa")));
+
+
+        //WAIT_BUYER_PAY 	交易创建，等待买家付款
+        if ($param['trade_status'] == 'WAIT_BUYER_PAY '){
+            $log->log(array("交易创建成功，等待买家付款","date: ".date("Y-m-d h:i:sa")));
+            exit("failure");
+        }
+
+        //根据支付状态trade_status，TRADE_SUCCESS或TRADE_FINISHED时，支付宝才会认定为买家付款成功。
+        if ($param['trade_status'] == 'TRADE_SUCCESS' || 'TRADE_FINISHED' ){
+            //交易成功，更改状态
+            $log->log(array("交易成功，更改订单状态","date: ".date("Y-m-d h:i:sa")));
+            exit("SUCCESS");
+        }
+
+        //TRADE_CLOSED     未付款交易超时关闭，或付完款全额退款
+        if ($param['trade_status'] == 'TRADE_CLOSED'){
+            //未付款超时，更改订单号，待重新发起支付
+            $log->log(array("未付款超时，更改订单号，待重新发起支付","date: ".date("Y-m-d h:i:sa")));
+            exit("failure");
+        }
+
     }else{
+        //验签失败
+        $log->log(array("验签失败","date: ".date("Y-m-d h:i:sa")));
         exit("failure");
     }
 
